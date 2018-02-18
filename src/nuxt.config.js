b61985c81d26c2770f640fa0b01129ac2d5f9dee
Attila Max Ruf <therufa@gmail.com>
2018-02-18 23:12:29 +0100

    now

---
+++ src/nuxt.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'koltra',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'marketplace for collectibles' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  buildDir: '../functions/nuxt',
+  build: {
+    publicPath: '/',
+    vendor: ['isomorphic-fetch'],
+    extractCSS: true,
+    babel: {
+      presets: [
+        'env',
+        'stage-0'
+      ],
+      plugins: [
+        ['transform-runtime', {
+          polyfill: true,
+          regenerator: true
+        }]
+      ]
+    },
+
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
---
+++ src/pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="fact in facts" :key="fact.text">
+        {{ fact.text }}
+      </li>
+    </ul>
+    <button @click="addItem">add +</button>
+  </div>
+</template>
+
+<script>
+import fetch from 'isomorphic-fetch'
+
+export default {
+  async asyncData() {
+    const response = await fetch('https://nuxt-ssr.firebaseio.com/facts.json')
+    const facts = await response.json()
+
+    return { facts }
+  },
+
+  methods: {
+    addItem() {
+      this.facts = [ ...this.facts, { text: `hello ${Math.random()}` } ]
+    }
+  },
+}
+</script>
+
+<style>
+</style>
